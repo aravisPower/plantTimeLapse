f72cc0f8736b3b8efda09d6635ae672ea035190a
Stéphane Mogenier <smogenier@etik.com>
2024-03-15 18:25:40 +0100

    Page d'accueil de l'interface de réglage (web)

---
+++ WebApp/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Time-Lapse - Raspberry Pi</title>
+</head>
+
+<body>
+	<div id="wrapper">
+		<header>
+		</header>
+
+		<section>
+			<h1>Réglages du time-lapse</h1>
+			<p>Les réglages suivant sont nécessaires avant de lancer le time-lapse. Après validation, la liste des régalges sera affichée et une confirmation sera demandée avant de lancer la séquence. Ensuite, un bouton permettra l'annulation en cours de réalisation.</p>
+			
+		</section>
+	</div>
+</body>
+
+</html>
